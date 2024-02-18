cb0380488318c63452f2d9d8535ae9f0c77164ce
Akhyar <akhyarakhyar725@gmail.com>
2024-02-18 14:48:38 +0700

    rnn optimazer

--- src/rnn/index.ts
+++ src/rnn/index.ts
@@ -2,149 +2,186 @@ import * as mj from "mathjs";
 import calculateActivation from "../activation";
 import calculateError from "../cost";
 import AdaGrad from "../optimazer/adaGrad";
+import Sgd from "../optimazer/sgd";
 import { activation } from "../activation/interfaces";
 import { cost } from "../cost/interfaces";
 import fs from "fs";
+import { optimazer, optimazerType } from "../optimazer/interfaces";
 
 export default class RNN {
   WIH: mj.Matrix;
+  WHH: mj.Matrix;
   WHO: mj.Matrix;
-  inputNodes: number;
-  hiddenNodes: number;
-  currentHidden: mj.Matrix;
-  seriesDataset: mj.Matrix[][];
-  optimazer: AdaGrad[];
-  activation: activation;
-  cost: cost;
-  lr: number = 0.1;
+  BI: mj.Matrix;
+  BO: mj.Matrix;
+  activation: activation = "lRelu";
+  lr: number;
+  private optimazerIH: optimazer = new Sgd();
+  private optimazerHH: optimazer = new Sgd();
+  private optimazerHO: optimazer = new Sgd();
+  private optimazerBI: optimazer = new Sgd();
+  private optimazerBO: optimazer = new Sgd();
+  private prev_hidden: mj.Matrix;
+  private outputs: mj.Matrix[] = [];
+  private dOutputs: mj.Matrix[] = [];
+  private hiddens: mj.Matrix[] = [];
+  private dHiddens: mj.Matrix[] = [];
+  private next_hidden: mj.Matrix = mj.matrix();
   constructor(
-    seriesDataset: mj.Matrix[][],
     inputNodes: number,
     hiddenNodes: number,
     outputNodes: number,
-    lr: number,
     activation: activation,
-    cost: cost
+    optimazer: optimazerType
   ) {
-    this.seriesDataset = seriesDataset;
-    this.inputNodes = inputNodes;
-    this.hiddenNodes = hiddenNodes;
-    this.WIH = this.generateMatrix([hiddenNodes, inputNodes + hiddenNodes]);
-    this.WHO = this.generateMatrix([outputNodes, hiddenNodes]);
-    this.optimazer = [
-      new AdaGrad(this.WIH.size()),
-      new AdaGrad(this.WHO.size()),
-    ];
-    this.currentHidden = mj.matrix(mj.zeros([hiddenNodes, 1]));
+    this.WIH = mj.matrix(mj.random([hiddenNodes, inputNodes], -1, 1));
+    this.WHH = mj.matrix(mj.random([hiddenNodes, hiddenNodes], -1, 1));
+    this.WHO = mj.matrix(mj.random([outputNodes, hiddenNodes], -1, 1));
+    this.BI = mj.matrix(mj.zeros([hiddenNodes, 1]));
+    this.BO = mj.matrix(mj.zeros([outputNodes, 1]));
+    this.prev_hidden = mj.matrix(mj.zeros([hiddenNodes, 1]));
+    this.next_hidden = mj.matrix(mj.zeros([hiddenNodes, 1]));
+    this.lr = 0.1;
     this.activation = activation;
-    this.cost = cost;
-    this.lr = lr;
+    if (optimazer === "adaGrad") {
+      this.optimazerIH = new AdaGrad([hiddenNodes, inputNodes]);
+      this.optimazerHH = new AdaGrad([hiddenNodes, hiddenNodes]);
+      this.optimazerHO = new AdaGrad([outputNodes, hiddenNodes]);
+      this.optimazerBI = new AdaGrad([hiddenNodes, 1]);
+      this.optimazerBO = new AdaGrad([outputNodes, 1]);
+    } else {
+      this.optimazerIH = new Sgd();
+      this.optimazerHH = new Sgd();
+      this.optimazerHO = new Sgd();
+      this.optimazerBI = new Sgd();
+      this.optimazerBO = new Sgd();
+    }
   }
 
+  predict(x: mj.Matrix[]) {
+    let xo: mj.Matrix = mj.matrix();
+    this.prev_hidden = mj.matrix(mj.zeros([this.WHH.size()[0], 1]));
+    for (let i in x) {
+      let xi = mj.multiply(this.WIH, x[i]);
+      let xh = mj.add(
+        mj.add(xi, mj.multiply(this.WHH, this.prev_hidden)),
+        this.BI
+      );
+      [xh] = calculateActivation(xh, "tanh");
+      this.prev_hidden = xh;
+
+      [xo] = calculateActivation(
+        mj.add(mj.multiply(this.WHO, xh), this.BO),
+        this.activation
+      );
+    }
+
+    return xo;
+  }
+
+  train(x: mj.Matrix[], y: mj.Matrix[]) {
+    let dXh = mj.matrix();
+    for (let i in x) {
+      let xi = mj.multiply(this.WIH, x[i]);
+      let xh = mj.add(
+        mj.add(xi, mj.multiply(this.WHH, this.prev_hidden)),
+        this.BI
+      );
+      [xh, dXh] = calculateActivation(xh, "tanh");
+      this.prev_hidden = xh;
+      this.hiddens[i] = xh;
+      this.dHiddens[i] = dXh;
+
+      let [xo, dHO] = calculateActivation(
+        mj.add(mj.multiply(this.WHO, xh), this.BO),
+        this.activation
+      );
+      this.outputs[i] = xo;
+      this.dOutputs[i] = dHO;
+    }
+
+    let [loss]: [mj.MathNumericType, mj.MathType] = [0, mj.matrix()];
+    let sumErrBO = mj.matrix(mj.zeros(this.outputs[0].size()));
+    let sumErrBI = mj.matrix(mj.zeros(this.hiddens[0].size()));
+    let gWHO = mj.matrix(mj.zeros(this.WHO.size()));
+    let gWHH = mj.matrix(mj.zeros(this.WHH.size()));
+    let gWIH = mj.matrix(mj.zeros(this.WIH.size()));
+    let indexBO = 0;
+    let indexBI = 0;
+
+    for (let i in y) {
+      let [l, err] = calculateError(y[i], this.outputs[i], "mse");
+      loss = mj.add(loss, l);
+      err = mj.dotMultiply(err, this.dOutputs[i]);
+      sumErrBO = mj.add(sumErrBO, err);
+      gWHO = mj.add(gWHO, mj.multiply(err, mj.transpose(this.hiddens[i])));
+      let gO = mj.multiply(mj.transpose(this.WHO), err);
+      let gH = mj.add(
+        gO,
+        mj.multiply(mj.transpose(this.WHH), this.next_hidden)
+      );
+      gH = mj.dotMultiply(gH, this.dHiddens[i]);
+      this.next_hidden = gH;
+
+      if (Number(i) > 0) {
+        gWHH = mj.add(
+          gWHH,
+          mj.multiply(gH, mj.transpose(this.hiddens[Number(i) - 1]))
+        );
+        sumErrBI = mj.add(sumErrBI, gH);
+        indexBI = Number(i);
+      }
+
+      gWIH = mj.add(gWIH, mj.multiply(gH, mj.transpose(x[i])));
+
+      indexBO = Number(i) + 1;
+    }
+    sumErrBO = mj.dotDivide(sumErrBO, indexBO);
+    sumErrBI = mj.dotDivide(sumErrBI, indexBI);
+    let gBO = this.optimazerBO.optimazer(this.lr, sumErrBO);
+    let gBI = this.optimazerBI.optimazer(this.lr, sumErrBI);
+    gWHO = this.optimazerHO.optimazer(this.lr, gWHO);
+    gWHH = this.optimazerHH.optimazer(this.lr, gWHH);
+    gWIH = this.optimazerIH.optimazer(this.lr, gWIH);
+
+    this.WHO = mj.subtract(this.WHO, gWHO);
+    this.WHH = mj.subtract(this.WHH, gWHH);
+    this.WIH = mj.subtract(this.WIH, gWIH);
+    this.BO = mj.subtract(this.BO, gBO);
+    this.BI = mj.subtract(this.BI, gBI);
+
+    return mj.dotDivide(loss, indexBO);
+  }
+
+  fit(X: mj.Matrix[][], y: mj.Matrix[][], epochs: number, path: string = "") {
+    for (let i = 0; i < epochs; i++) {
+      for (let j in X) {
+        const loss = this.train(X[j], y[j]);
+        path === "" ? null : this.saveModels(path);
+        console.clear();
+        console.log("epoch =>", i);
+        console.log("loss =>", loss);
+      }
+    }
+  }
   saveModels(path: string) {
     const data = {
       WIH: this.WIH.toJSON(),
       WHO: this.WHO.toJSON(),
-      // BI: this.BI.toJSON(),
-      // BH: this.BH.map((B) => B.toJSON()),
-      // BO: this.BO.toJSON(),
+      WHH: this.WHH.toJSON(),
+      BI: this.BI.toJSON(),
+      BO: this.BO.toJSON(),
     };
     const dataJson = JSON.stringify(data);
     fs.writeFileSync(path, dataJson);
   }
-
   loadModels(path: string) {
     const dataJson = fs.readFileSync(path, "utf-8");
     const result = JSON.parse(dataJson);
     this.WIH = mj.matrix(result.WIH.data);
     this.WHO = mj.matrix(result.WHO.data);
-    // this.BI = mj.matrix(result.BI.data);
-    // this.BO = mj.matrix(result.BO.data);
-  }
-
-  generateMatrix(shape: number[]): mj.Matrix {
-    return mj.matrix(mj.random(shape, -1, 1));
-  }
-
-  predict(X_series: mj.Matrix[]): mj.Matrix {
-    let pred = mj.matrix();
-    this.currentHidden = mj.matrix(mj.zeros([this.hiddenNodes, 1]));
-
-    for (let X of X_series) {
-      let inputs = mj.concat(mj.flatten(X), mj.flatten(this.currentHidden));
-      inputs = mj.reshape(inputs, [this.inputNodes + this.hiddenNodes, 1]);
-      const [RIH, dIH] = calculateActivation(
-        mj.multiply(this.WIH, inputs),
-        "tanh"
-      );
-      const [HO, dHO] = calculateActivation(
-        mj.multiply(this.WHO, RIH),
-        this.activation
-      );
-      pred = HO;
-      this.currentHidden = RIH;
-    }
-    return pred;
-  }
-
-  train(X: mj.Matrix, y: mj.Matrix) {
-    let inputs = mj.concat(mj.flatten(X), mj.flatten(this.currentHidden));
-    inputs = mj.reshape(inputs, [this.inputNodes + this.hiddenNodes, 1]);
-    const target = y;
-    const [RIH, dIH] = calculateActivation(
-      mj.multiply(this.WIH, inputs),
-      "tanh"
-    );
-    const [HO, dHO] = calculateActivation(
-      mj.multiply(this.WHO, RIH),
-      this.activation
-    );
-
-    const [loss, err] = calculateError(target, HO, this.cost);
-
-    const d_err_o = mj.dotMultiply(err, dHO);
-    const d_err_o_lr = mj.multiply(d_err_o, 1);
-    const gHO = mj.multiply(d_err_o_lr, mj.transpose(RIH));
-    const newWHO = mj.subtract(
-      this.WHO,
-      this.optimazer[1].optimazer(this.lr, gHO)
-    );
-
-    this.WHO = newWHO;
-
-    const err_ih = mj.multiply(mj.transpose(this.WHO), err);
-    const d_err_ih = mj.dotMultiply(err_ih, dIH);
-    const d_err_ih_lr = mj.multiply(d_err_ih, 1);
-    const gIH = mj.multiply(d_err_ih_lr, mj.transpose(inputs));
-    const newWIH = mj.subtract(
-      this.WIH,
-      this.optimazer[0].optimazer(this.lr, gIH)
-    );
-
-    this.WIH = newWIH;
-    this.currentHidden = RIH;
-    return loss;
-  }
-
-  fit(epoch: number, savePath: string = "") {
-    let l;
-    for (let i = 0; i < epoch; i++) {
-      for (let dataset of this.seriesDataset) {
-        for (let series in dataset) {
-          if (dataset[Number(series) + 1] !== undefined) {
-            const loss = this.train(
-              dataset[series],
-              dataset[Number(series) + 1]
-            );
-            l = loss;
-          }
-          console.clear();
-          console.log("epoch =>", i);
-          console.log("loss =>", l);
-        }
-        this.currentHidden = mj.matrix(mj.zeros([this.hiddenNodes, 1]));
-        if (savePath !== "") this.saveModels(savePath);
-      }
-    }
+    this.WHH = mj.matrix(result.WHH.data);
+    this.BI = mj.matrix(result.BI.data);
+    this.BO = mj.matrix(result.BO.data);
   }
 }
